d7055824bf03a6cbb792509d5248feab0d52ee36
branch10480 <toshiharu.imaeda@gmail.com>
2024-05-08 23:41:54 +0900

    first commit

---
+++ src/main/kotlin/com/example/beanusagesample/BeanUsageSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.beanusagesample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeanUsageSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeanUsageSampleApplication>(*args)
+}
